3a5c26e1b511b339e94337e75c1dd7f362217a2b
James Draper <5493761+JamesDraper@users.noreply.github.com>
2026-03-13 22:03:34 +0000

    Added outline of test case class and test attribute class.

---
+++ src/Attributes/Test.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Spaghetti;
+
+use Attribute;
+
+#[Attribute]
+final readonly class Test
+{
+}
---
+++ src/TestCase.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Spaghetti;
+
+abstract class TestCase
+{
+    final public function __construct()
+    {
+    }
+
+    final public function run(): void
+    {
+        // todo: finish this.
+    }
+
+    protected function setUp(): void
+    {
+    }
+
+    protected function tearDown(): void
+    {
+    }
+}
